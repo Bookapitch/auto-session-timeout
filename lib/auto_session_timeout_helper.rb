c7d2d1985a3da4ef4669531a2a626614b9156349
Thilo Rusche <thilorusche@gmail.com>
2021-02-17 15:56:50 +0000

    namespaced support for javascript helper

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -2,6 +2,8 @@ module AutoSessionTimeoutHelper
   def auto_session_timeout_js(options={})
     frequency = options[:frequency] || 60
     attributes = options[:attributes] || {}
+    path_to_timeout = send([options[:namespace], "timeout_path"].compact.join("_"))
+    path_to_active = send([options[:namespace], "active_path"].compact.join("_"))
     code = <<JS
 function PeriodicalQuery() {
   var request = new XMLHttpRequest();
@@ -9,10 +11,10 @@ function PeriodicalQuery() {
     var status = event.target.status;
     var response = event.target.response;
     if (status === 200 && (response === false || response === 'false' || response === null)) {
-      window.location.href = '#{timeout_path}';
+      window.location.href = '#{path_to_timeout}';
     }
   };
-  request.open('GET', '#{active_path}', true);
+  request.open('GET', '#{path_to_active}', true);
   request.responseType = 'json';
   request.send();
   setTimeout(PeriodicalQuery, (#{frequency} * 1000));
